4efeaf90051f93c5ba5eb7665d8b2a7f494d5053
TheNewsYorkTime <asalawless5c@gmail.com>
2022-02-17 18:29:48 -0800

    update

--- article.js
+++ article.js
@@ -1,6 +1,6 @@
 function load() {
     var file = new XMLHttpRequest();
-    file.open("GET", "http://remote.tld/random.txt", true);
+    file.open("GET", "article.txt", true);
     file.onreadystatechange = function() {
       if (file.readyState === 4) {  // Makes sure the document is ready to parse
         if (file.status === 200) {  // Makes sure it's found the file
